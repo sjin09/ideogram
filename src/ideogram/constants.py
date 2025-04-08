93c94f33cb222d7e5b8a63d0cba189ecb446890f
sjin09 <sl17@sanger.ac.uk>
2025-04-08 21:42:15 +0900

    init

---
+++ src/ideogram/constants.py
@@ -0,0 +1,11 @@
+
+CYTOBAND_COLOURS = {
+    "gneg": "#FFFFFF",  # white
+    "gpos25": "#D3D3D3",  # light gray
+    "gpos50": "#A9A9A9",  # medium gray
+    "gpos75": "#696969",  # dark gray
+    "gpos100": "#000000",  # black
+    "acen": "#B22222"  # red
+    "gvar": "#E0E0E0",  # pale silver
+    "stalk": "#C0C0C0"  # silver
+}
